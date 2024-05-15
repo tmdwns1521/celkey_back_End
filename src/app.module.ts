e3be4b6f428e6da94bcbe430c690da99e726badb
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-05-15 21:34:49 +0900

    회원가입 api 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module' // AuthModule 추가
+import { AuthModule } from './auth/auth.module'; // AuthModule 추가
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
