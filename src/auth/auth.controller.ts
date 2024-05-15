0448d5db3e539a7059ab3d3260aed5fb57e0db58
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-05-16 03:18:52 +0900

    AccessToken api 미들웨어 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Request, Response } from "express";
 import { AuthService } from "./auth.service";
@@ -22,6 +22,13 @@ export class AuthController {
   @Get('kakao/callback')
   @UseGuards(AuthGuard('kakao'))
   async kakaoAuthCallback(@Req() req: Request, @Res() res: Response) {
+    const tokens = await this.authService.validateOAuthLogin(req);
+    console.log('tokens ::: ', tokens);
+    res.json(tokens);
+  }
+
+  @Post('kakao/refresh-token')
+  async kakaoAuthRefreshToken(@Req() req: Request, @Res() res: Response) {
     const tokens = await this.authService.validateOAuthLogin(req);
     console.log('tokens ::: ', tokens);
     res.json(tokens);
