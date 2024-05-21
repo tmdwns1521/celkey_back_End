c0c76e47463d9190689d931fb64d025fd0064291
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-05-22 00:42:35 +0900

    리프레시 토큰 재발급 API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
     credentials: true, // credentials 허용
   });
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('CelKey API Documentation')
+    .setDescription('celkey API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
