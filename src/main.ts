916a2e7a32d5800223dbd4aa412ed403a2aff10f
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-05-21 16:19:32 +0900

    리프레시 토큰 저장

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: 'http://localhost:8000',
     credentials: true, // credentials 허용
   });
   await app.listen(3000);
